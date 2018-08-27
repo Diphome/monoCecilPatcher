4ebed548d6544376a50667ce607b08db07632bb7
Diphome <edouard.puel@gmail.com>
2018-08-27 18:34:44 +0200

    added project

---
+++ MonoCecilPatcher/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace MonoCecilPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Assembly to patch";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(201, 184);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 4;
+            this.textBox7.Tag = "Assembly class name";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(201, 240);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 5;
+            this.textBox8.Tag = "Assembly method name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Class name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(198, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Method name";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(201, 132);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(253, 22);
+            this.textBox9.TabIndex = 11;
+            this.textBox9.Tag = "Assembly path";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(474, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 27);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 40);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Patch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(198, 296);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Dll to patch with";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(201, 371);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 15;
+            this.textBox10.Tag = "Dll class name";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(201, 427);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Tag = "Dll method name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(198, 351);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Class name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 407);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Method name";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(201, 316);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(253, 22);
+            this.textBox5.TabIndex = 19;
+            this.textBox5.Tag = "Dll path";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(474, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 27);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 567);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(196, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(242, 25);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "MonoCecil Patcher v1.0";
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label10.Location = new System.Drawing.Point(201, 278);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(346, 2);
+            this.label10.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label11.Location = new System.Drawing.Point(201, 95);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(346, 2);
+            this.label11.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(198, 45);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(328, 40);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Patch an existing assembly with code from\r\nyour own dll.";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(442, 254);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(105, 21);
+            this.checkBox3.TabIndex = 28;
+            this.checkBox3.Text = "save values";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(442, 444);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 21);
+            this.checkBox1.TabIndex = 29;
+            this.checkBox1.Text = "save values";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(201, 468);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 2);
+            this.label1.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(628, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MonoCecil Patcher v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MonoCecilPatcher/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoCecilPatcher.Properties;
+
+namespace MonoCecilPatcher
+{
+    public partial class Form1 : Form
+    {
+        public static bool saveAssemblyValues = false;
+        public static bool saveDllValues = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "dll files (*.dll) | *.dll";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox5.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Textbox not empty check.
+            foreach (Control x in this.Controls)
+            {
+                if (x is TextBox)
+                {
+                    if (string.IsNullOrWhiteSpace(x.Text))
+                    {
+                        MessageBox.Show("The field " + x.Tag + " is empty,\nplease fill it.", "Empty field",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+
+            string dllPath = textBox5.Text;
+            string assemblyPath = textBox9.Text;
+            string dllClassName = textBox10.Text;
+            string dllMethodName = textBox6.Text;
+            string assemblyClassName = textBox7.Text;
+            string assemblyMethodName = textBox8.Text;
+
+            //Because often assemblies are linked, we move in the assembly directory to avoid error file not found.
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(assemblyPath));
+
+            //Because if you launched the patched version without the dll containing the code in the same directory
+            //it won't work, we check that the dll is in the directory and if not we copy it in.
+            string dllInAssemblyDirectory = Path.GetDirectoryName(assemblyPath) + "\\" + Path.GetFileName(dllPath);
+            File.Copy(dllPath, dllInAssemblyDirectory, true);
+            
+
+            //Getting module.
+            var injassembly = AssemblyDefinition.ReadAssembly(dllPath);
+
+            //Setting module method that will replace the assembly one.
+            var typedefinj = injassembly.MainModule.Types.Single(t => t.Name == dllClassName);
+            var injmethDefinition = typedefinj.Methods.Single(t => t.Name == dllMethodName);
+
+            //Getting assembly.
+            var assembly = AssemblyDefinition.ReadAssembly(assemblyPath);
+
+            //Setting assembly method that will be replaced by the module one.
+            var typeDefinition = assembly.MainModule.Types.Single(t => t.Name == assemblyClassName);
+            var methDefinition = typeDefinition.Methods.Single(t => t.Name == assemblyMethodName);
+            
+            var setMethodWriter = methDefinition.Body.GetILProcessor();
+            var firstExistingInstruction = setMethodWriter.Body.Instructions[0];
+
+            //Injecting in assembly method beginning the module method.
+            setMethodWriter.InsertBefore(firstExistingInstruction, setMethodWriter.Create(OpCodes.Call, assembly.MainModule.Import(injmethDefinition.Resolve())));
+
+            string patchPath = "";
+            switch (Path.GetExtension(assemblyPath))
+            {
+                case ".exe":
+                    //If it is an executable we replace it directly with a new one name-patched.exe
+                    patchPath = Path.Combine(Path.GetDirectoryName(assemblyPath), Path.GetFileNameWithoutExtension(assemblyPath)) + "-patched" + Path.GetExtension(assemblyPath);
+                    break;
+                case ".dll":
+                    //Else we need to replace the.dll and backup the original one naming it name.orig.dll
+                    patchPath = assemblyPath;
+                    break;
+            }
+
+
+            try
+            {
+                assembly.Write(patchPath);
+                if(MessageBox.Show("Assembly sucessfully patched, you can find it here :\n" + patchPath, "Patch done !",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(assemblyPath));
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Something went wrong, here is the exception caught :\n" + ex.ToString(), "Error patching",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox9.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            saveAssemblyValues = !saveAssemblyValues;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (saveAssemblyValues)
+            {
+                Settings.Default.assemblyPath = textBox9.Text;
+                Settings.Default.assemblyClassName = textBox7.Text;
+                Settings.Default.assemblyMethodName = textBox8.Text; 
+            }
+            if (saveDllValues)
+            {
+                Settings.Default.dllPath = textBox5.Text;
+                Settings.Default.dllClassName = textBox10.Text;
+                Settings.Default.dllMethodName = textBox6.Text;
+            }
+
+            Settings.Default.rememberAssembly = checkBox3.Checked;
+            Settings.Default.rememberDll = checkBox1.Checked;
+            Settings.Default.Save();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            saveDllValues = !saveDllValues;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.dllPath != null)
+            {
+                textBox5.Text = Settings.Default.dllPath;
+                textBox10.Text = Settings.Default.dllClassName;
+                textBox6.Text = Settings.Default.dllMethodName;
+                checkBox1.Checked = Settings.Default.rememberDll;
+            }
+            if(Settings.Default.assemblyPath != null)
+            {
+                textBox9.Text = Settings.Default.assemblyPath;
+                textBox7.Text = Settings.Default.assemblyClassName;
+                textBox8.Text = Settings.Default.assemblyMethodName;
+                checkBox3.Checked = Settings.Default.rememberAssembly;
+            }
+        }
+    }
+}
